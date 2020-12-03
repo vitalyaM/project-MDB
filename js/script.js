a9eb3a42dbc00ff6c0870d908e673a46b44da60b
Vitalii Sukhorukov <vitaliysuhorukov123@gmail.com>
2020-12-03 17:16:57 +0300

    finish: part 1

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@
 Реализовать только при помощи JS
 
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+Отсортировать их по алфавиту
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+"use strict";
 
 const movieDB = {
     movies: [
@@ -24,3 +24,28 @@ const movieDB = {
     ]
 };
 
+const advUnits = document.querySelectorAll(".promo__adv img"),
+    promoContent = document.querySelector(".promo__content"),
+    promoGenre = promoContent.querySelector(".promo__genre"),
+    promoBg = promoContent.querySelector(".promo__bg"),
+    watchedMovies = promoContent.querySelector(".promo__interactive-list");
+
+advUnits.forEach(unit => {
+    unit.remove();
+});
+
+promoGenre.textContent = "драма";
+promoBg.style.backgroundImage = "url('../img/bg.jpg')";
+
+watchedMovies.innerHTML = '';
+
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+    watchedMovies.insertAdjacentHTML(
+        'beforeend',
+        `<li class="promo__interactive-item">
+            ${i + 1}: ${film}
+            <div class="delete"></div>
+        </li>`
+    );
+});
